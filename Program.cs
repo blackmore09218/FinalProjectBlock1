99577abf777c4c3540c0179038bd20592c5f35f9
IvanZakharov <blackmore09218@yandex.ru>
2022-12-06 23:01:42 +0300

    Дописали и проверили программу, выводящую массив из символов, длина которых меньше либо равна 3 символа

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
     for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
     Console.WriteLine();
 }
+
+Console.WriteLine("Введите элементы строкового массива через пробел: ");
+string arrayStr = Console.ReadLine();
+string[] array = arrayStr.Split(" ");
+string resultArrayStr = "";
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resultArrayStr = resultArrayStr.Insert(resultArrayStr.Length, array[i]);
+        resultArrayStr = resultArrayStr.Insert(resultArrayStr.Length, " ");
+    }
+}
+string[] resultArray = resultArrayStr.Split(" ");
+PrintArray(resultArray);
